2cfd945f58ba17e5250069199734c987dff9daba
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-05-23 19:56:40 -0400

    adding old tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+// registers an event handler, which calculates the  length of input value in the textarea, and console logs the value.
+$(document).ready(function () {
+  $("#tweet-text").on("input", function() {
+    console.log("this", $(this).parent());
+    const textareaNumber = $(this).val();
+    const textareaLength = textareaNumber.length;
+    const remainingText = 140 - textareaLength;
+    const counterText = $(this).parent().children(".characteristics").children(".counter");
+    counterText.text(remainingText);
+    
+    // generates the visuals (e.g. colour) for textarea limit based on remaining characters
+    if (remainingText < 0) {
+      counterText.addClass("red");
+    } else {
+      counterText.removeClass("red");
+    }
+  });
+});
